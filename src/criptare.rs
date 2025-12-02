c635b678ee502e51e7cd748cd465ab9f61b8ceb9
Bereșteanu Mihail <beresteanumihail29@gmail.com>
2025-12-02 14:38:06 +0200

    Realizare etapa handshake
    Am implementat functie pentru generarea cheilor publice / private, iar cu ajutorul functii derive_key ajung la o parola comuna intre server si client

--- src/criptare.rs
+++ src/criptare.rs
@@ -1,3 +1,47 @@
-fn main() {
-    
+use aes_gcm::aead::OsRng;
+
+use x25519_dalek::{EphemeralSecret, PublicKey};
+
+use sha2::{Digest, Sha256};
+
+//Am nevoie de o structura sa retin cheia secreta pana realizam Handshake-ul
+pub struct RememberSecret {
+    secret_key: EphemeralSecret,
+    pub public_key: PublicKey,
+}
+
+impl Default for RememberSecret {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl RememberSecret {
+    pub fn new() -> Self {
+        //generez o cheie secreta random
+        let secret_key = EphemeralSecret::random_from_rng(OsRng);
+        //derivez cheia publica in functie de cea privata
+        let public_key = PublicKey::from(&secret_key);
+
+        Self {
+            secret_key,
+            public_key,
+        }
+    }
+
+    pub fn derive_key(self, public_key_bytes: Vec<u8>) -> [u8; 32] {
+        let key_expected: [u8; 32] = public_key_bytes
+            .try_into()
+            .expect("Cheia publica trebuie sa aiba 32 bytes!");
+
+        //Convertim array-ul intr-o cheie publica
+        let aux_key = PublicKey::from(key_expected);
+
+        let shared_secret = self.secret_key.diffie_hellman(&aux_key);
+
+        let mut hash_func = Sha256::new();
+        hash_func.update(shared_secret.as_bytes());
+
+        hash_func.finalize().into()
+    }
 }
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,57 +1,56 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct MessageHistoryInfo{
+pub struct MessageHistoryInfo {
     pub message_id: u64,
     pub sender: String,
     pub content: String,
     pub time: i64,
-    pub delivered: bool
+    pub delivered: bool,
 }
 
 //Fac un enunm corespunzator protocolului propus
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Message{
+pub enum Message {
     //Realizarea handshake-ului
 
     //Clientul trimite cheia publica catre server
-    ClientKey{
+    ClientKey {
         public_key: Vec<u8>,
     },
 
     //Serverul raspunde cu cheia publica
-    ServerKey{
+    ServerKey {
         public_key: Vec<u8>,
     },
 
     //Autentificare
-    Login{
+    Login {
         username: String,
         password: String,
     },
 
     //Clientul trimite mesajul catre server(in caz ca raspunde la un mesaj retin ID-ul )
-    Text{
+    Text {
         to: String,
         content: String,
         reply_id: Option<u64>,
-
     },
 
     //Mesajele sunt trimise de catre server(retin timpul mesajelor trimise in caz ca sunt offline pentru a fi distribuite in ordinea corecta)
-    ToSend{
+    ToSend {
         from: String,
         content: String,
         time: u64,
     },
 
     //Istoricul pe care il vreau de la o persoana respectiva
-    HistoryInfo{
+    HistoryInfo {
         user: String,
     },
 
     //Retin mesajele trimise pentru a afisa istoricul
-    HistoryData{
+    HistoryData {
         content: Vec<MessageHistoryInfo>,
     },
 }
