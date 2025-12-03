40ca94810ddfb6139d462121a396fd0062b4e01f
Bereșteanu Mihail <beresteanumihail29@gmail.com>
2025-12-03 10:07:35 +0200

    Tables create for users and messages

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use rusqlite::{Connection, Result};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let conn = Connection::open("info.db")?;
+
+    //Realizez 2 baze de date pentru userii conectati dar si informatiile mesajelor transmise
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS
+                        users(username TEXT PRIMARY KEY,
+                              password TEXT NOT NULL)",
+        (),
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS
+                        messages(sender TEXT NOT NULL,
+                                 receiver TEXT NOT NULL,
+                                 id INTEGER PRIMARY KEY,
+                                 content TEXT NOT NULL,
+                                 time INTEGER NOT NULL,
+                                 delivered INTEGER DEFAULT 0)",
+        (),
+    )?;
+
+    Ok(())
+}
